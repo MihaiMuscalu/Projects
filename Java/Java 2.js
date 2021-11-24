7c44f2c2b05b8e0cc0f463b4c016bea202e02916
Mihai <mihai.muscalu@student.htldornbirn.at>
2021-11-24 15:11:28 +0100

    Class Start

--- Java/Java 2.js
+++ Java/Java 2.js
@@ -8,37 +8,29 @@ console.log("The result of " + a + " divided by " + b + " is " + result);
 let firstname = "Mihai";
 let lastname = "Muscalu";
 
-console.log("Ich heiße " + firstname+ " " + lastname);
+console.log("Ich heiße " + firstname + " " + lastname);
 
-for(let count = 7;count<45;count++)
-{
+for (let count = 7; count < 45; count++) {
     console.log(count);
 }
-for(let count = 9;count<99;count++)
-{
+for (let count = 9; count < 99; count++) {
     console.log(count);
 }
-for(let count = 10;count>0;count--)
-{
+for (let count = 10; count > 0; count--) {
     console.log(count);
 }
-for(let count = 1234;count>1206;count--)
-{
+for (let count = 1234; count > 1206; count--) {
     console.log(count);
 }
-for(let count = 0;count<8;count++)
-{
+for (let count = 0; count < 8; count++) {
     console.log("Mihai");
 }
-for(let count = 0;count<11;count++)
-{
+for (let count = 0; count < 11; count++) {
     console.log(count);
-    
-    if(count>=10)
-    {
-        console.log("Mihai")   
-        for(let count = 10;count>0;count--)
-        {
+
+    if (count >= 10) {
+        console.log("Mihai")
+        for (let count = 10; count > 0; count--) {
             console.log(count);
         }
     }
